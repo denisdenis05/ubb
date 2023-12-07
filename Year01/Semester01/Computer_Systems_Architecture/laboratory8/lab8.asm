f32798c580079d4baf76cf38efbb47c9249618a7
denisdenis05 <66214092+denisdenis05@users.noreply.github.com>
2023-12-08 01:47:11 +0200

    Add files via upload

---
+++ Year01/Semester01/Computer_Systems_Architecture/laboratory8/lab8.asm
@@ -0,0 +1,69 @@
+bits 32 
+
+
+global start        
+
+extern exit, scanf, fopen, fprintf, fclose
+import exit msvcrt.dll   
+import scanf msvcrt.dll
+import fopen msvcrt.dll
+import fprintf msvcrt.dll
+import fclose msvcrt.dll
+
+;Read two numbers a and b (base 10) from the keyboard and calculate: (a+b)*(a-b). The result of multiplication will be stored in a variable called "result" (defined in the data segment).
+
+segment data use32 class=data
+    a dw 0
+    b dw 0
+    format dd "%d", 0 
+    result dd 0
+    file db "output.txt", 0
+    readtype db "a+", 0
+    file_descriptor db 0
+    
+segment code use32 class=code
+    start:
+        startoftheloop:
+            push a
+            push format
+            call [scanf]
+            add esp, 4*2
+        
+            mov ecx, 0
+            check_lowercase:
+                mov al, [a + ecx] 
+                cmp al, 0   
+                je end_loop
+
+                cmp al, 'a'  
+                jg lowercase
+                
+                cmp al, 'z'  
+                jl lowercase
+
+                jmp end_loop
+                inc ecx
+                jmp check_lowercase
+            
+            
+            lowercase:
+            push file
+            push readtype
+            call [fopen]
+            add esp, 4*2
+            mov [file_descriptor], eax
+            push dword a
+            push dword [file_descriptor]
+            call [fprintf]
+            add esp, 4*2
+            push dword [file_descriptor]
+            call [fclose]
+            add esp, 4
+
+                
+            end_loop:
+        
+        
+        
+        push    dword 0   
+        call    [exit]   
---
+++ Year01/Semester01/Computer_Systems_Architecture/laboratory8/lab8_2.asm
@@ -0,0 +1,26 @@
+bits 32 
+
+
+global start        
+
+extern exit, scanf, printf
+import exit msvcrt.dll   
+import scanf msvcrt.dll
+import printf msvcrt.dll
+
+;Read numbers (in base 10) in a loop until the digit '0' is read from the keyboard. Determine and display the biggest number from those that have been read.
+
+segment data use32 class=data
+    a dd 0
+    format dd "%d", 0 
+    max dd 0
+    printresult db "The biggest number is %d", 0
+
+
+segment code use32 class=code
+    start:
+        mov al, 2
+        add al, al
+        
+        push    dword 0   
+        call    [exit]   
