5fb0a35c186fc002b5fd451579adab7a7d12ed91
denisdenis05 <66214092+denisdenis05@users.noreply.github.com>
2023-12-08 01:44:26 +0200

    Add files via upload

---
+++ Year01/Semester01/Computer_Systems_Architecture/laboratory5/loops.asm
@@ -0,0 +1,67 @@
+bits 32 
+
+global start        
+
+extern exit             
+import exit msvcrt.dll    
+
+
+;A byte string S is given. Obtain the string D1 which contains the elements found on the even positions of S and the string D2 which contains the elements found on the odd positions of S.
+;Example:
+;S: 1, 5, 3, 8, 2, 9
+;D1: 1, 3, 2
+;D2: 5, 8, 9
+
+segment data use32 class=data
+    s db 1, 5, 3, 8, 2, 9
+    slength equ $-s
+    
+    x dd 2
+    
+    d1 times slength db 0
+    d2 times slength db 0
+    
+    
+
+segment code use32 class=code
+    start:
+    
+        mov esi, 0
+        mov edi, 0
+        
+        mov ecx, 0
+        mov cl, slength ;ecx= lengths1
+        
+        startofiteration1:
+            
+            mov ebx, 0
+            mov bl, slength
+            sub ebx, ecx 
+            
+            mov edx, 0
+            mov eax, 0
+            mov eax, ebx
+            div dword [x]
+            test edx, edx
+            jnz oddposition 
+            
+                
+            
+            mov al, [s+ebx]
+            mov [d2+esi], al
+            add esi, 1
+            jmp endofadding
+            
+            
+            oddposition:
+                mov al, [s+ebx]
+                mov [d1+edi], al
+                add edi, 1
+                
+                endofadding:
+                    sub ecx, 1
+                    test ecx, ecx
+                    jnz startofiteration1
+    
+        push    dword 0    
+        call    [exit]       
---
+++ Year01/Semester01/Computer_Systems_Architecture/laboratory5/loops2.asm
@@ -0,0 +1,65 @@
+bits 32 
+
+global start        
+
+extern exit              
+import exit msvcrt.dll   
+
+segment data use32 class=data
+    s1 db '+', '4', '2', 'a', '8', '4', 'X', '5'
+    lengths1 equ $-s1 
+    s2 db 'a', '4', '5'
+    lengths2 equ $-s2
+    d db 0
+    x dd 3
+
+segment code use32 class=code
+    start:
+        
+        mov esi, -1
+        mov ecx, 0
+        mov cl, lengths1 ;ecx= lengths1
+        
+        startofiteration1:
+            
+            mov ebx, 0
+            mov bl, [d+esi] ;display???
+            
+            mov ebx, 0
+            mov bl, lengths1
+            sub ebx, ecx 
+            
+            mov edx, 0
+            mov eax, 0
+            mov eax, ebx
+            div dword [x]
+            test edx, edx
+            jnz endofadding
+            
+                
+            add esi, 1
+            mov al, [s1+ebx]
+            mov [d+esi], al
+            
+            endofadding:
+            
+                sub ecx, 1
+                test ecx, ecx
+                jnz startofiteration1
+        
+        mov ecx, 0
+        mov cl, lengths2 
+        sub ecx, 1 ;ecx= lengths2-1
+        startofiteration2:
+            
+            mov ebx, 0
+            mov bl, [d+esi] ;display???
+            
+            mov al, [s2+ecx]
+            mov [d+esi], al
+            add esi, 1
+            sub ecx, 1
+            jnz startofiteration2
+            
+        push    dword 0  
+        call    [exit]      
